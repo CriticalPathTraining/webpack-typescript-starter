381cdbe154270a676a8a915866b989775d3dcfe7
Ted Pattison <ted@tedpattison.net>
2019-01-13 14:12:04 -0500

    Updates

---
+++ src/scripts/app.ts
@@ -0,0 +1,11 @@
+﻿import './../css/app.css';
+
+window.onload = () => {
+  console.log("window.onload...");
+  let container: HTMLElement = document.getElementById("content-box");
+  container.innerHTML = "Hello World for the Webpack-TypeScript Starter Project";
+};
+
+window.onresize = () => {
+  console.log("window.onresize...");
+};
